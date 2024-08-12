092f67986f9f639a97e5725b56f4dc7294602234
nelsonbs <nelsonboche@gmail.com>
2024-08-12 16:23:37 -0600

    Conexion a Azure

--- views/paginas/cliente.php
+++ views/paginas/cliente.php
@@ -333,9 +333,6 @@ $usuario_id = $_SESSION['id_usuario'];
   </div>
 
 </div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8=" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.5.0-beta4/html2canvas.min.js"></script>
 <script>
     $(document).ready(() => {
         var canvasDiv = document.getElementById('canvasDiv');
